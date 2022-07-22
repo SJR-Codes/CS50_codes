2094b380b804e79ab19c1f58cd7313c92a02094f
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-22 07:37:02 +0000

    Fri, Jul 22, 2022, 7:37 AM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -6,10 +6,12 @@
 
 
 fname = input("Enter file name: ").strip().lower()
+known_exts = ["gif", "jpg", "jpeg", "png", "pdf", "txt", "zip"]
+
 
 if "." in fname:
     ext = fname.split(".")
-    
+    if ext in known_exts:
     print(ext[1])
 else:
     print("application/octet-stream")
