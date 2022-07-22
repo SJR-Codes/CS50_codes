2e083698690a39e99753091a84c0b9883d1037a6
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-22 09:17:43 +0000

    Fri, Jul 22, 2022, 9:17 AM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -9,17 +9,18 @@ def main():
     utime = input("What time is it? ").strip()
 
     ctime = convert(utime)
-
+    print(ctime)
+"""
     match ctime:
         case 7:
             print("Breakfast")
-        case 
-
+        case
+"""
 
 def convert(time):
     tmp = time.split(":")
 
-    return int(tmp[0])
+    return float(tmp[0] + (tmp[1]/60))
 
 
 if __name__ == "__main__":
