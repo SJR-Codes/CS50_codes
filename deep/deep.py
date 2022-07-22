cba260033201391f729aaeacdc688c6400eaba03
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-22 07:00:54 +0000

    Fri, Jul 22, 2022, 7:00 AM +00:00

---
+++ deep/deep.py
@@ -0,0 +1,13 @@
+"""
+* CS50P Problem Set 1
+* Deep Thought
+* by Samu Reinikainen 22.07.2022
+"""
+
+
+answer = input("What is the answer to the meaning of life etc.? ")
+
+if int(answer) == 42 or lower(answer) == "forty-two" or lower(answer) == "forty two":
+    print("Yes")
+else:
+    print("No")
