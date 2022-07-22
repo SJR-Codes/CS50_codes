d1c4b3e19dbade1cc6a76f3db0bd33786b18b761
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-22 08:41:23 +0000

    Fri, Jul 22, 2022, 8:41 AM +00:00

---
+++ interpreter/interpreter.py
@@ -0,0 +1,20 @@
+"""
+* CS50P Problem Set 1
+* Math Interpreter
+* by Samu Reinikainen 22.07.2022
+"""
+
+main():
+    #operands
+    ops = ["+", "-", "*", "/"]
+    uinp = input("Enter your homework: ").split(" ")
+
+    if uinp[0].isnumeric() and uinp[2].isnumeric() and uinp[1] in ops:
+
+    else:
+        print("")
+
+
+
+
+main()
