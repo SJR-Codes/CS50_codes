099dceeb0e45b25a590789e9b1667838d11122b5
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-22 09:05:05 +0000

    Fri, Jul 22, 2022, 9:05 AM +00:00

---
+++ meal/meal.py
@@ -0,0 +1,17 @@
+"""
+* CS50P Problem Set 1
+* Meal Time
+* by Samu Reinikainen 22.07.2022
+"""
+
+
+def main():
+    ...
+
+
+def convert(time):
+    ...
+
+
+if __name__ == "__main__":
+    main()
