7935b109c30a1b84276788bfa7e4e8beefeafb4a
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-22 07:38:51 +0000

    Fri, Jul 22, 2022, 7:38 AM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -12,6 +12,8 @@ known_exts = ["gif", "jpg", "jpeg", "png", "pdf", "txt", "zip"]
 if "." in fname:
     ext = fname.split(".")
     if ext in known_exts:
-    print(ext[1])
+        print(ext[1])
+    else:
+        print("application/octet-stream")
 else:
     print("application/octet-stream")
