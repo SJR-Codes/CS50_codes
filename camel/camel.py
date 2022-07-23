d76be38704155bce454884af1f9365685cc524c7
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-23 16:41:03 +0000

    Sat, Jul 23, 2022, 4:41 PM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -4,3 +4,13 @@
 * by Samu Reinikainen 23.07.2022
 """
 
+def main():
+    varname = input("Enter variable name: ")
+
+    varname = snakify(varname)
+
+
+def snakify(vname):
+    #convert string from camel case to snake case
+
+    
