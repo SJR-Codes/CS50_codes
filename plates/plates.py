a2e2d2ce32a63d25c7b2139049f426fcc43b285e
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-23 18:19:44 +0000

    Sat, Jul 23, 2022, 6:19 PM +00:00

---
+++ plates/plates.py
@@ -0,0 +1,29 @@
+"""
+* CS50P Problem Set 2
+* Vanity Plates
+* by Samu Reinikainen 23.07.2022
+"""
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    #“All vanity plates must start with at least two letters.”
+    #“… vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.”
+    #“Numbers cannot be used in the middle of a plate; they must come at the end. For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a ‘0’.”
+    #“No periods, spaces, or punctuation marks are allowed.”
+
+
+def has_two_start(s):
+    #check that first two chars are letters
+
+def has_corr_amount(s):
+    #check that string len is 2-6
+    return 2 <= s.len() <= 6
+
+main()
