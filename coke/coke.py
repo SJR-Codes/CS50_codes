ee080fad997ad89cb47b73643f98f85ac2347d0d
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-23 17:03:33 +0000

    Sat, Jul 23, 2022, 5:03 PM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -7,12 +7,14 @@
 def main():
     coke_price = 50
     accept_change = [25, 10, 5]
-    while money < coke_price:
+    amount = 0
+
+    while amount < coke_price:
         change = int(input("Enter money(25c, 10c or 5c): "))
         if change in accept_change:
-            money += change
+            amount += change
+        print("Amount due: " + str(money))
 
-    
 
 
 
