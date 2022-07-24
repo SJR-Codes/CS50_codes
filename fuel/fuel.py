406c2db7c66193afe0e3b17c69d3ddf5af5fba83
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-24 11:55:41 +0000

    Sun, Jul 24, 2022, 11:55 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,7 +1,7 @@
 """
 * CS50P Problem Set 2
-* Nutrition Facts
-* by Samu Reinikainen 23.07.2022
+* Fuel Gauge
+* by Samu Reinikainen 24.07.2022
 """
 
 def main():
@@ -38,6 +38,12 @@ def check_input(fract):
 def fract_to_gauge(fract):
     #convert fract to gauge, ie. 3/4 = 75%
     res = int(int(fract[0]) / int(fract[1]) * 100)
+
+    if res <= 1:
+        res = "E"
+    elif res >= 99:
+        res = "F"
+
     print(res)
 
 
