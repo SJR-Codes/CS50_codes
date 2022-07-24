895be3a2c6e473c092a75e38bc761f3635784171
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-24 12:31:02 +0000

    Sun, Jul 24, 2022, 12:31 PM +00:00

---
+++ taqueria/taqueria.py
@@ -0,0 +1,22 @@
+"""
+* CS50P Problem Set 3
+* Fuel Gauge
+* by Samu Reinikainen 24.07.2022
+"""
+
+def main():
+
+
+
+menu = 
+{
+    "Baja Taco": 4.00,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
