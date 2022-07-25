48264c6aa29ca948a0b7feba5e1dc9052c6f821a
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-25 15:21:38 +0000

    Mon, Jul 25, 2022, 3:21 PM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -8,9 +8,18 @@ import sys
 import json
 import requests as r
 
+if len(sys.argv) != 2:
+    sys.exit("Error: Argument missing!")
+try:
+    coins = float(sys.argv[1])
+except ValueError:
+    sys.exit("Error: Argument not float!")
 
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 try:
-    ...
+    response = r.get(url)
 except requests.RequestException:
     ...
+
+"""
