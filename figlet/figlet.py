8d77016ff8efe8a722d6ff93f6778b521e36df98
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-25 09:50:07 +0000

    Mon, Jul 25, 2022, 9:50 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,17 +5,20 @@
 """
 
 import sys
-import random
+import random AS r
 from pyfiglet import Figlet
 
 figlet = Figlet()
 #You can then get a list of available fonts with code like this:
 fonts = figlet.getFonts()
 print(fonts)
-"""
+
 if len(sys.argv) == 0:
-    font = fonts[]
-elif len(sys.argv) == 2:
+    f = choise(fonts)
+elif len(sys.argv) == 2 and (sys.argv[1] == "-f" or sys.argv[1] == "--font"):
+    f = sys.argv[1]
+else:
+    sys.exit("Invalid usage")
 
 #You can set the font with code like this, wherein f is the font’s name as a str:
 figlet.setFont(font=f)
@@ -23,4 +26,3 @@ figlet.setFont(font=f)
 #And you can output text in that font with code like this, wherein s is that text as a str:
 print(figlet.renderText(s))
 
-"""
