6372388d7b0128221bcf95b3cfd5df4eb4bc8ef1
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-25 09:42:45 +0000

    Mon, Jul 25, 2022, 9:42 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,3 +5,16 @@
 """
 
 import sys
+import random
+from pyfiglet import Figlet
+
+figlet = Figlet()
+#You can then get a list of available fonts with code like this:
+
+fonts = figlet.getFonts()
+
+#You can set the font with code like this, wherein f is the font’s name as a str:
+figlet.setFont(font=f)
+
+#And you can output text in that font with code like this, wherein s is that text as a str:
+print(figlet.renderText(s))
