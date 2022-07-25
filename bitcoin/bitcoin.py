be48d3b2834a114df68dafe8dbe5de84bea5797d
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-25 15:17:56 +0000

    Mon, Jul 25, 2022, 3:17 PM +00:00

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,16 @@
+"""
+* CS50P Problem Set 4
+* Bitcoin Price Index
+* by Samu Reinikainen 25.07.2022
+"""
+
+import sys
+import json
+import requests as r
+
+
+
+try:
+    ...
+except requests.RequestException:
+    ...
