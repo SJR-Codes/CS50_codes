92fa9768b305d7b036e30a91c17e50852d97a361
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 09:26:05 +0000

    Tue, Jul 26, 2022, 9:26 AM +00:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -1,2 +1,14 @@
 from bank import value
 
+def test_100():
+    assert value("  Hello") == 100
+    assert value("Yello") == 100
+
+def test_20():
+    assert value("Hi") == 20
+    assert value("How are you") == 20
+
+def test_0():
+    assert value("Hello") == 0
+    assert value("Hello, sir") == 0
+
