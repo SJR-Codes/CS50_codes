8b2a7eb41fcc79cf55e737f2796fc89d2b949629
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 07:18:13 +0000

    Tue, Jul 26, 2022, 7:18 AM +00:00

--- week_5/test_calc.py
+++ week_5/test_calc.py
@@ -12,8 +12,31 @@ def test_square():
     if square(3) != 9:
         print("Error: 3 squared != 9")
     """
-    assert square(2) == 4
-    assert square(3) == 9
+
+    """
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("Error: 2 squared != 4")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("Error: -2 squared != 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("Error: 3 squared != 9")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("Error: -3 squared != 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("Error: 0 squared != 0")
+    """
+
+    #pytest
 
 if __name__ == "__main__":
     main()
