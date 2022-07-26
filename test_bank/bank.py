0fc0df99db7aee8a06b10c7f15082052bf6949e1
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 09:22:32 +0000

    Tue, Jul 26, 2022, 9:22 AM +00:00

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -5,16 +5,16 @@
 """
 
 def main():
-    print(value(input("Greetings: ").lower()))
+    print("$" + str(value(input("Greetings: ").lower())))
 
 
-def value(greeting):
+def value(greet):
     if greet[0:5] == "hello":
-        return "$0"
+        return "0"
     elif greet[0] == "h":
-        return "$20"
+        return "20"
     else:
-        return "$100"
+        return "100"
 
 
 if __name__ == "__main__":
