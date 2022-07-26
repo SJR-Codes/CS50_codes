9d4aa80a64a1fd3561da7b42c04f6ea32b09023b
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 10:46:04 +0000

    Tue, Jul 26, 2022, 10:46 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -2,12 +2,37 @@ def main():
     ...
 
 
-def convert(fraction):
-    ...
+def convert(fract):
+    if(fract.count("/") == 1):
+        fract = fract.split("/")
+    else:
+        return False
 
+    try:
+        x = int(fract[0])
+        y = int(fract[1])
+    except ValueError:
+        #print("Value Error: Not valid fraction!")
+        return False
+
+    if y <= 0 or x < 0 or x > y:
+        #print("Zero or Negative Number Error: Not valid fraction!")
+        return False
+
+    return fract
 
 def gauge(percentage):
-    ...
+    #convert fract to gauge, ie. 3/4 = 75%
+    res = round(int(fract[0]) / int(fract[1]) * 100)
+
+    if res <= 1:
+        res = "E"
+    elif res >= 99:
+        res = "F"
+    else:
+        res = str(res) + "%"
+
+    return res
 
 
 if __name__ == "__main__":
