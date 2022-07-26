63af33366ea412e55932df091d724dd366d93c70
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 07:11:53 +0000

    Tue, Jul 26, 2022, 7:11 AM +00:00

--- week_5/test_calc.py
+++ week_5/test_calc.py
@@ -1,15 +1,19 @@
-import square from calc
+from calc import square
 
 def main():
     test_square()
 
     print("All tests run.")
 
-def test_square:
+def test_square():
+    """
     if square(2) != 4:
         print("Error: 2 squared != 4")
     if square(3) != 9:
         print("Error: 3 squared != 9")
+    """
+    assert square(2) == 4
+    assert square(3) == 9
 
 if __name__ == "__main__":
     main()
