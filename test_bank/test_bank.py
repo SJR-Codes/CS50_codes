30633568a1086628c3ff08eaa1ab2ad72eed2540
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 09:30:05 +0000

    Tue, Jul 26, 2022, 9:30 AM +00:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -12,3 +12,14 @@ def test_0():
     assert value("Hello") == 0
     assert value("Hello, sir") == 0
 
+def test_100_low():
+    assert value("  hello") == 100
+    assert value("yello") == 100
+
+def test_20_low():
+    assert value("hi") == 20
+    assert value("how are you") == 20
+
+def test_0_low():
+    assert value("hello") == 0
+    assert value("hello, sir") == 0
