786dd6834eaa974d250705337fb50603e3a73bea
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 09:21:20 +0000

    Tue, Jul 26, 2022, 9:21 AM +00:00

---
+++ test_bank/bank.py
@@ -0,0 +1,25 @@
+"""
+* CS50P Problem Set 1
+* Home Federal Savings Bank
+* by Samu Reinikainen 22.07.2022
+"""
+
+def main():
+    print(value(input("Greetings: ").lower()))
+
+
+def value(greeting):
+    if greet[0:5] == "hello":
+        return "$0"
+    elif greet[0] == "h":
+        return "$20"
+    else:
+        return "$100"
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
