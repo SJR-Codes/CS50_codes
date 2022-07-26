b5db9960eec278e4d042dc781c654e6418da75b9
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 11:06:56 +0000

    Tue, Jul 26, 2022, 11:06 AM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -11,7 +11,7 @@ def test_convert():
     assert fuel.convert("1/1") == 100
     assert fuel.convert("1/2") == 50
     assert fuel.convert("0/2") == 0
-    assert fuel.convert("2/0") == 0
+    assert fuel.convert("99/100") == 99
 
 def test_gauge():
     assert fuel.gauge(100) == "F"
@@ -19,6 +19,17 @@ def test_gauge():
     assert fuel.gauge(99) == "F"
     assert fuel.gauge(1) == "E"
     assert fuel.gauge(50) == "50%"
+    assert fuel.gauge(2) == "2%"
+    assert fuel.gauge(98) == "98%"
 
 def test_zero_division():
-    
+    with pytest.raises(ZeroDivisionError):
+        assert fuel.convert("2/0")
+
+def test_invalid_values():
+    with pytest.raises(ValueError):
+        assert fuel.convert("4/2")
+    with pytest.raises(ValueError):
+        assert fuel.convert("cat")
+    with pytest.raises(ValueError):
+        assert fuel.convert("4/")
