fa75c39dcccd1eea6638987c5a6eb3f8c59ebbc8
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-26 08:41:34 +0000

    Tue, Jul 26, 2022, 8:41 AM +00:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -1,7 +1,7 @@
 """
-* CS50P Problem Set 2
-* Just setting up my twttr
-* by Samu Reinikainen 23.07.2022
+* CS50P Problem Set 5
+* Just testing my twttr
+* by Samu Reinikainen 26.07.2022
 """
 
 def main():
