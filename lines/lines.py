8b95979de6653973cddb2711bd913c9a03c748d8
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-27 11:48:41 +0000

    Wed, Jul 27, 2022, 11:48 AM +00:00

---
+++ lines/lines.py
@@ -0,0 +1,13 @@
+"""
+* CS50P Problem Set 6
+* Lines of Code
+* by Samu Reinikainen 27.07.2022
+"""
+
+import sys
+
+try:
+    filen = sys.argv[1]
+except IndexError:
+    sys.exit("Enter filename.")
+
