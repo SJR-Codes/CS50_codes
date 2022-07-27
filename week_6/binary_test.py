4adb0cb1ce279b1e400a4b034d651e41183164b6
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-27 11:25:29 +0000

    Wed, Jul 27, 2022, 11:25 AM +00:00

--- week_6/binary_test.py
+++ week_6/binary_test.py
@@ -4,3 +4,19 @@
 * by Samu Reinikainen 27.07.2022
 """
 
+import sys
+from PIL import Image
+
+images = []
+
+#remember slicing
+for arg in sys.argv.[1:]:
+    image = Image.open(arg)
+    images.append(image)
+
+images[0].save(
+    "costumes.gif",
+    save_all=True,
+    append_images[images[1]],
+    duration=200, loop=0]
+)
