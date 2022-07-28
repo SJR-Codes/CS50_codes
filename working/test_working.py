788399d20d466c2e84c30a939fac01a82c2d615e
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 15:09:52 +0000

    Thu, Jul 28, 2022, 3:09 PM +00:00

--- working/test_working.py
+++ working/test_working.py
@@ -12,7 +12,7 @@ def test_valid_times():
     assert convert("9:00 AM to 5:00 PM") == "9:00 to 17:00"
     assert convert("9 AM to 5 PM") == "9:00 to 17:00"
     assert convert("9 AM to 5:00 PM") == "9:00 to 17:00"
-    assert convert("8 AM to 4 PM") == "8:00 to 16:00"
+    assert convert("8:35 AM to 4:45 PM") == "8:35 to 16:45"
 
 
 def test_invalid_times():
