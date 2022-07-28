267bf7a3ac567a54d571dd743ca403bbb179d790
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 08:12:59 +0000

    Thu, Jul 28, 2022, 8:12 AM +00:00

--- week_7/notes.py
+++ week_7/notes.py
@@ -4,15 +4,23 @@
 * by Samu Reinikainen 28.07.2022
 """
 
+import re
+
 email = input("Give me your email: ").strip()
 
-username, domain = email.split("@")
-
-if len(username) > 1 and "." in domain:
+if re.search("@", email):
     print("Valid")
 else:
     print("Invalid")
 
+#username, domain = email.split("@")
+
+"""
+if len(username) > 1 and "." in domain:
+    print("Valid")
+else:
+    print("Invalid")
+"""
 """
 if "@" in email and "." in email:
     print("Valid")
