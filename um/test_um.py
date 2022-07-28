36eba8c00f00e4c6d8003e9a8e01de166ce2d561
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 16:04:54 +0000

    Thu, Jul 28, 2022, 4:04 PM +00:00

--- um/test_um.py
+++ um/test_um.py
@@ -10,7 +10,7 @@ from um import count
 def test_simple():
     assert count("Regular, um, Expressions") == 1
     assert count("Regular, um, um, Expressions") == 2
-    assert count("Regular, UM, Um, Expressions") == 2
+    assert count("Regular, UM, Um, Expressions um...") == 3
     assert count("It’s um not uncommon, in English, at least, to say “um” when trying to, um, think of a word. The more you do it, though, the more noticeable it tends to be!") == 3
 
 
