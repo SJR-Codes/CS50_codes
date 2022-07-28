b946e82bea6c48ff6b531c9401924bfe61a50568
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 10:02:59 +0000

    Thu, Jul 28, 2022, 10:02 AM +00:00

--- week_7/format.py
+++ week_7/format.py
@@ -9,9 +9,16 @@ import re
 name = input("Give me your name: ").strip()
 
 #David Malan or Malan, David
-
+"""
 if ", " in name:
     lname, fname = name.split(", ")
+    name = f"{fname} {lname}"
+"""
+
+matches = re.search("^(\w+), ?(\w+)$", name)
+if matches:
+    lname, fname = matches.groups()
+    print(matches)
     name = f"{fname} {lname}"
 
 print(f"Hello, {name}")
