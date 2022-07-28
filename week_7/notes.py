49bf6a20d83ac784bca6704a3eb5d50609a28ce7
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 07:04:56 +0000

    Thu, Jul 28, 2022, 7:04 AM +00:00

--- week_7/notes.py
+++ week_7/notes.py
@@ -1,12 +1,12 @@
 """
 * CS50P Problem Set 7
-* Notes
+* Notes for regexps
 * by Samu Reinikainen 28.07.2022
 """
 
 email = input("Give me your email: ").strip()
 
-if "@" in email:
+if "@" in email and "." in email:
     print("Valid")
 else:
     print("Invalid")
