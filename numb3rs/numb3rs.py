20e89fa6f62e7c94ef9e3637af7f003601c136f1
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 11:35:08 +0000

    Thu, Jul 28, 2022, 11:35 AM +00:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -3,14 +3,14 @@ import sys
 
 
 def main():
-    print(validate(input("IPv4 Address: ")))
+    print(validate(input("IPv4 Address: ").strip()))
 
 
 def validate(ip):
-    ...
-
-
-...
+    if re.search("^[0-255]\.[0-255]\.[0-255]\.[0-255]\$", ip):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
