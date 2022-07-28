f8e91052ab06f02e9e88235b096e57b965defc1c
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 15:06:48 +0000

    Thu, Jul 28, 2022, 3:06 PM +00:00

--- working/working.py
+++ working/working.py
@@ -17,16 +17,16 @@ def convert(s):
     #p = "^([0-1]?[0-9]:?(?:[0-5][0-6])?.[AM|PM]) to ([0-1]?[0-9]:?(?:[0-5][0-6])?.[AM|PM])$"
     #p = "^([0-9][0-9]:?[0-9][0-9].[AM|PM]) to ([0-9][0-9]:?[0-9][0-9].[AM|PM])$"
     p = r"^([0-1]?[0-9](?::[0-5][0-9])? [A|P]M) to ([0-1]?[0-9](?::[0-5][0-9])? [A|P]M)$"
-    try:
-        if m := re.search(p, s, re.IGNORECASE):
-            start = convert_time(m.group(1))
-            end = convert_time(m.group(2))
+    #try:
+    if m := re.search(p, s, re.IGNORECASE):
+        start = convert_time(m.group(1))
+        end = convert_time(m.group(2))
 
-            return start + " to " + end
-        else:
-            raise ValueError
-    except ValueError:
-        pass
+        return start + " to " + end
+    else:
+        raise ValueError
+    #except ValueError:
+    #    pass
         #sys.exit()
 
 #converts 05:00 PM or 5 PM to 17:00
