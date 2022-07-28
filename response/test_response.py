d7a8d7de34ed59dd38a4d62913817c4049c57d0f
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 16:38:01 +0000

    Thu, Jul 28, 2022, 4:38 PM +00:00

---
+++ response/test_response.py
@@ -0,0 +1,18 @@
+"""
+* CS50P Problem Set 7
+* Test Um
+* by Samu Reinikainen 28.07.2022
+"""
+
+import pytest
+from response import check
+
+def test_valid():
+    assert check("ad@ad.fi") == "Valid"
+    assert check("asd@sub.ad.edu") == "Valid"
+    assert check("foo.bar@test.edu") == "Valid"
+
+def test_invalid():
+    assert check("ad@adfi") == "Invalid"
+    assert check("asd@sub.ad.") == "Invalid"
+    assert check("foo.bar@") == "Invalid"
