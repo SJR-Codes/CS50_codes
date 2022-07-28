4634e31ddc949495fba442f0e1f934d453aa3c00
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 16:34:49 +0000

    Thu, Jul 28, 2022, 4:34 PM +00:00

--- response/response.py
+++ response/response.py
@@ -4,8 +4,20 @@
 * by Samu Reinikainen 28.07.2022
 """
 
+from validator_collection import validators, errors
+
+def main():
+    #print(validate(input("What's your email address? ").strip()))
+    print(check("ad@ad.fi"))
 
 
-email = input("What's your email address? ").strip()
+def check(e):
+
+    if validators.email(e, allow_empty = False):
+        return "Valid"
+    else:
+        return "Invalid"
 
 
+if __name__ == "__main__":
+    main()
