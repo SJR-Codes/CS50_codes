3b29ac0c743c849056ba72cf95b2b9d46497f985
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-28 14:29:16 +0000

    Thu, Jul 28, 2022, 2:29 PM +00:00

--- working/working.py
+++ working/working.py
@@ -14,15 +14,16 @@ def main():
 
 def convert(s):
     #09:00 AM to 05:00 PM
-    p = "^([0-1]?[0-9]:?(?:[0-5][0-6])?.[AM|PM]) to ([0-1]?[0-9]:?(?:[0-5][0-6])?.[AM|PM])$"
-    try:
-        if m := re.search(p, s, re.IGNORECASE):
-            print(m.group(1))
-            print(m.group(2))
-        else:
-            raise ValueError
-    except ValueError:
-        pass
+    #p = "^([0-1]?[0-9]:?(?:[0-5][0-6])?.[AM|PM]) to ([0-1]?[0-9]:?(?:[0-5][0-6])?.[AM|PM])$"
+    p = "^([0-9]*[AM|PM]) to ([0-9]*[AM|PM])$"
+    #try:
+    if m := re.search(p, s, re.IGNORECASE):
+        print(m.group(1))
+        print(m.group(2))
+    else:
+        raise ValueError
+    #except ValueError:
+    #    pass
     #    sys.exit()
 
 
