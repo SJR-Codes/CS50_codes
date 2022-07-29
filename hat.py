eba24885def3c7a8b0bf2bf0c26e293b60c5ca85
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-29 10:07:25 +0000

    Fri, Jul 29, 2022, 10:07 AM +00:00

--- hat.py
+++ hat.py
@@ -1,17 +1,24 @@
 import random
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Foo","Bar","Huu","Haa"]
+    #def __init__(self):
+    #    self.houses = ["Foo","Bar","Huu","Haa"]
+    #class variables
+    houses = ["Foo","Bar","Huu","Haa"]
 
-    def sort(self, name):
-        print(name, "is in", random.choice(self.houses))
+#    def sort(self, name):
+#        print(name, "is in", random.choice(self.houses))
 
 
     #class methods
-    #@classmethod
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
 
 
-hat = Hat()
+#
+#hat = Hat()
+#hat.sort("Harry")
 
-hat.sort("Harry")
+#when using class methods you can use class without initializing
+Hat.sort("Harry")
