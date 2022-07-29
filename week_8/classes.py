82c5da306fae1eb4d32ff7c6ce1542a6468fc2f5
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-29 08:04:28 +0000

    Fri, Jul 29, 2022, 8:04 AM +00:00

--- week_8/classes.py
+++ week_8/classes.py
@@ -4,16 +4,19 @@
 * by Samu Reinikainen 29.07.2022
 """
 
-class 
+class Student:
+    ...
 
 def main():
     student = get_student()
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {"name": name, "house": house}
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+
+    return student
 
 if __name__ == "__main__":
     main()
