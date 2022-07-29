53ad2efdaa71640a7c7f6cc542206c5bde89e22f
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-29 13:35:05 +0000

    Fri, Jul 29, 2022, 1:35 PM +00:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -17,6 +17,7 @@ def main():
 def get_bday():
     #bday = input("Your birthday (YYYY-MM-DD): ").strip()
     bday = "1999-05-05"
+    bday = "2021-07-29"
     #bday = "1999-05-"
     if re.search("^[\d][\d][\d][\d]-[0-1][\d]-[0-3][\d]$", bday):
         return bday
@@ -26,6 +27,8 @@ def get_bday():
 def dates_to_minutes(date_start, date_end):
     ds = date.fromisoformat(date_start)
     de = date_end
+
+    print(f"{ds} - {de}")
 
 
 if __name__ == "__main__":
