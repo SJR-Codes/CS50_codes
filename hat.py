06087bc1e09bd0dc85ffedca57edd41c82c74bb5
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-29 09:58:35 +0000

    Fri, Jul 29, 2022, 9:58 AM +00:00

--- hat.py
+++ hat.py
@@ -1,6 +1,17 @@
+import random
+
 class Hat:
-    ...
+    def __init__(self):
+        self.houses = ["Foo","Bar","Huu","Haa"]
+
+    def sort(self, name):
+        print(name, "is in", random.choice(self.houses))
+
+
+    #class methods
+    #@classmethod
 
 
 hat = Hat()
 
+hat.sort("Harry")
