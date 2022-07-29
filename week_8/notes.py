8ad09080d84f54d5a5e633583438e5c8edd745ba
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-29 07:43:46 +0000

    Fri, Jul 29, 2022, 7:43 AM +00:00

--- week_8/notes.py
+++ week_8/notes.py
@@ -7,15 +7,13 @@
 
 def main():
     student = get_student()
-    name = student[0]
-    name = "Foobar"
     print(f"{student[0]} from {student[1]}")
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
 
-    #return two values as tuple (immutable)
+    #return two values as tuple (inmutable)
     return (name, house)
 
 if __name__ == "__main__":
