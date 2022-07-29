57b7f62d40114cb39182e6a8efbd5fa880f23148
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-29 13:12:10 +0000

    Fri, Jul 29, 2022, 1:12 PM +00:00

---
+++ seasons/seasons.py
@@ -0,0 +1,5 @@
+"""
+* CS50P Problem Set 8
+* Seasons of Love
+* by Samu Reinikainen 29.07.2022
+"""
---
+++ seasons/test_seasons.py
@@ -0,0 +1,19 @@
+"""
+* CS50P Problem Set 8
+* Test Seasons of Love
+* by Samu Reinikainen 29.07.2022
+"""
+
+import sys
+from datetime import date
+
+
+def main():
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
