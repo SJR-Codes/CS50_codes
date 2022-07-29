c787bf9dff35827c2cb1d2aaa3fe766922d54d4b
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-29 08:17:30 +0000

    Fri, Jul 29, 2022, 8:17 AM +00:00

--- week_8/classes.py
+++ week_8/classes.py
@@ -4,8 +4,16 @@
 * by Samu Reinikainen 29.07.2022
 """
 
+#
+#class Student:
+#    ...
+
 class Student:
-    ...
+    #instance method to initialize
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
 
 def main():
     student = get_student()
