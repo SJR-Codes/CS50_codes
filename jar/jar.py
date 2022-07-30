4b60696d45b49d72dfd60959cea697b89a222380
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-30 10:06:12 +0000

    Sat, Jul 30, 2022, 10:06 AM +00:00

--- jar/jar.py
+++ jar/jar.py
@@ -3,3 +3,36 @@
 * Cookie Jar
 * by Samu Reinikainen 30.07.2022
 """
+
+class Jar:
+    def __init__(self, capacity=12):
+        if capacity > 0:
+            self.amount = 0
+            self.capacity = 0
+        else:
+            raise ValueError
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
