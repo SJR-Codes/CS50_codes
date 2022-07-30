e3101fde217a858ee30c6b1e73d4ff52288f7905
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-30 11:19:59 +0000

    Sat, Jul 30, 2022, 11:19 AM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -7,7 +7,7 @@
 from fpdf import FPDF
 
 def make_shirt(name)
-    pdf = FPDF()
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
     pdf.set_font("helvetica", "B", 16)
     pdf.cell(40, 10, name)
