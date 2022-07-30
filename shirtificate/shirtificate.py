8c009fc48ca3d9a8756eca0cb7bea29437f71d58
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-30 11:46:06 +0000

    Sat, Jul 30, 2022, 11:46 AM +00:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -12,7 +12,10 @@ def make_shirt(name):
     pdf.add_page()
     pdf.set_font("helvetica", "B", 32)
     pdf.cell(0, 0, "CS50 Shirtificate", align="C", new_x="LEFT", new_y="NEXT")
+    pdf.set_y(60)
     pdf.image("shirtificate.png", w=pdf.epw, h=0)
+    pdf.set_y(120)
+    pdf.cell(0, 0, name, align="C", new_x="LEFT", new_y="NEXT")
     pdf.output("shirtificate.pdf")
 
 def main():
