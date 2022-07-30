213369f9dc14ef6b8a97dff91b95248f3f5ead75
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-30 10:02:13 +0000

    Sat, Jul 30, 2022, 10:02 AM +00:00

---
+++ jar/jar.py
@@ -0,0 +1,5 @@
+"""
+* CS50P Problem Set 8
+* Cookie Jar
+* by Samu Reinikainen 30.07.2022
+"""
---
+++ jar/test_jar.py
@@ -0,0 +1,26 @@
+"""
+* CS50P Problem Set 8
+* Test Cookie Jar
+* by Samu Reinikainen 30.07.2022
+"""
+
+class Jar:
+    def __init__(self, capacity=12):
+        ...
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
