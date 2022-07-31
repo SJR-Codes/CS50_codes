cc4a42801b6e5daeed506bad433523bdf166ff4a
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 10:36:39 +0000

    Sun, Jul 31, 2022, 10:36 AM +00:00

--- args.py
+++ args.py
@@ -1,6 +1,6 @@
 """
 * CS50P Problem Set 8
-* Notes week 9: using command line arguments
+* Notes week 9: using named command line arguments
 * by Samu Reinikainen 30.07.2022
 """
 
@@ -9,5 +9,9 @@ import sys
 
 if len(sys.argv) == 1:
     print("miau")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("Miau!")
 else:
     print("Usage: args.py -n 3")
