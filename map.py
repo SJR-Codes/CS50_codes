99a4a6b32b25c9efe5576aaba00103bd283f2665
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 11:27:42 +0000

    Sun, Jul 31, 2022, 11:27 AM +00:00

--- map.py
+++ map.py
@@ -1,6 +1,30 @@
 """
 * CS50P Problem Set 8
-* Notes week 9: datatype map
+* Notes week 9: function map, functional programming
 * by Samu Reinikainen 30.07.2022
 """
 
+def main():
+    #yell("This is CS50")
+    #yell(["This", "is", "CS50"])
+    yell("This", "is", "CS50")
+
+
+#def yell(phrase):
+#    print(phrase.upper())
+"""
+def yell(words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+"""
+
+def yell(*words):
+    uppercased = []
+    for word in words:
+        uppercased.append(word.upper())
+    print(*uppercased)
+
+if __name__ == "__main__":
+    main()
