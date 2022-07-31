34706f2f82f28b8d219b1d46cdb508809e17573d
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 10:11:51 +0000

    Sun, Jul 31, 2022, 10:11 AM +00:00

---
+++ docstrings.py
@@ -0,0 +1,23 @@
+"""
+* CS50P Problem Set 8
+* Notes week 9: Docstrings
+* by Samu Reinikainen 30.07.2022
+"""
+
+#document functions with docstrings
+def miau(n: int) -> str:
+    """
+    Returns string "Miau! " n number of times.
+
+    :param n: Number of times to Miau
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of n Miaus! on one line
+    :rtype: str
+    """
+    return "Miau! " * n
+
+numb: int = int(input("Number: "))
+miaus: str = miau(numb)
+
+print(miaus)
