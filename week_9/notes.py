81637c87a68adcaeae7f542002da45b2b8f19c7f
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 08:06:17 +0000

    Sun, Jul 31, 2022, 8:06 AM +00:00

---
+++ week_9/notes.py
@@ -0,0 +1,35 @@
+"""
+* CS50P Problem Set 8
+* week 9 notes
+* by Samu Reinikainen 30.07.2022
+"""
+
+#datatype: set
+
+students = [
+    {"name": "Hermione", "house": "Gryffindor"},
+    {"name": "Harry", "house": "Gryffindor"},
+    {"name": "Ron", "house": "Gryffindor"},
+    {"name": "Draco", "house": "Slytherin"},
+    {"name": "Padma", "house": "Ravenclaw"},
+]
+
+"""
+houses = []
+
+for student in students:
+    if student["house"] not in houses:
+        houses.append(student["house"])
+
+for house in sorted(houses):
+    print house
+"""
+
+#easier with sets
+houses = set()
+
+for student in students:
+        houses.add(student["house"])
+
+for house in sorted(houses):
+    print(house)
