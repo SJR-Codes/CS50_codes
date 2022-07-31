b9dee40a80680abcdf505d8826c4d78234f9eb85
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 09:43:55 +0000

    Sun, Jul 31, 2022, 9:43 AM +00:00

--- miaus.py
+++ miaus.py
@@ -4,8 +4,20 @@
 * by Samu Reinikainen 30.07.2022
 """
 
+"""
+#no constans in python to prevent MIAUS changed after declaring
 MIAUS = 3
 
 for _ in range(MIAUS):
     print("Miau!")
+"""
 
+class Cat:
+    MIAUS = 3
+
+    def miau(self):
+        for _ in range(Cat.MIAUS):
+            print("Miau!")
+
+cat = Cat()
+cat.miau()
