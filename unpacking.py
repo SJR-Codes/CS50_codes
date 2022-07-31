02d01cf6c53677ca497c28c97de4f6314d9aa206
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 11:04:38 +0000

    Sun, Jul 31, 2022, 11:04 AM +00:00

--- unpacking.py
+++ unpacking.py
@@ -14,11 +14,23 @@ print(f"Hello, {first}")
 
 def total(galleons, sickles, knuts):
     return (galleons * 17 +  sickles) * 29 + knuts
-
+"""
 coins = [100,50,25]
 
 #the hard way
 #print(total(coins[0],coins[1],coins[2]), "Knuts")
 
-#unpacking way with *
+#unpacking list way with *
 print(total(*coins), "Knuts")
+"""
+
+#named variables
+print(total(galleons=100, sickles=50, knuts=25), "Knuts")
+
+coins = {"sickles": 50, "galleons": 100, "knuts": 25}
+
+#hard way
+print(total(coins["galleons"], coins["sickles"], coins["knuts"]), "Knuts")
+
+#unpacking dictionary to named variables with **
+print(total(**coins), "Knuts")
