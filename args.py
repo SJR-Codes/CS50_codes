74fdce5f8350b94f6c1d4fbcf73ad0cb4f236933
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 10:40:42 +0000

    Sun, Jul 31, 2022, 10:40 AM +00:00

--- args.py
+++ args.py
@@ -4,10 +4,28 @@
 * by Samu Reinikainen 30.07.2022
 """
 
+"""
 import sys
 
 
 if len(sys.argv) == 1:
+    print("miau")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("Miau!")
+else:
+    print("Usage: args.py -n 3")
+"""
+
+#same with argparse
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add__argument("-n")
+args = parser.parse_args()
+
+if len(args) == 1:
     print("miau")
 elif len(sys.argv) == 3 and sys.argv[1] == "-n":
     n = int(sys.argv[2])
