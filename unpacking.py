09e8d64e0bf8d929e6cea4c59a74aee0c36a3a1b
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 10:55:01 +0000

    Sun, Jul 31, 2022, 10:55 AM +00:00

--- unpacking.py
+++ unpacking.py
@@ -4,4 +4,21 @@
 * by Samu Reinikainen 30.07.2022
 """
 
-#unpackin variables
+#unpacking variables to multiple vars
+
+"""
+#split return two but you want to use just one
+first, _ = input("Name").split(" ")
+print(f"Hello, {first}")
+"""
+
+def total(galleons, sickles, knuts):
+    return (galleons * 17 +  sickles) * 29 + knuts
+
+coins = [100,50,25]
+
+#the hard way
+#print(total(coins[0],coins[1],coins[2]), "Knuts")
+
+#unpacking way
+print(total(coins), "Knuts")
