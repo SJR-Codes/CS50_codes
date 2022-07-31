81e2495a5bd99f1d8f7468191c9caa0b982bdb68
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 08:22:12 +0000

    Sun, Jul 31, 2022, 8:22 AM +00:00

--- week_9/bank.py
+++ week_9/bank.py
@@ -4,10 +4,12 @@
 * by Samu Reinikainen 30.07.2022
 """
 
+"""
 #set variable to global
 balance = 0
 
 def main():
+    #you can print global variable without declaring it global
     print("Balance:", balance)
     deposit(100)
     print("Balance:", balance)
@@ -15,12 +17,36 @@ def main():
     print("Balance:", balance)
 
 def deposit(n):
+    #to change global variable declare it global
     global balance
     balance += n
 
 def withdraw(n):
     global balance
     balance -= n
+"""
+
+class Account():
+    def __init__(self):
+        self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, n):
+        self._balance += n
+
+    def withdraw(self, n):
+        self._balance -= n
+
+def main():
+    acc = Account()
+    print("Balance:", acc.balance)
+    acc.deposit(100)
+    print("Balance:", acc.balance)
+    acc.withdraw(50)
+    print("Balance:", acc.balance)
 
 if __name__ == "__main__":
     main()
