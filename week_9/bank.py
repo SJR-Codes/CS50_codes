40b9380d9bcbe6d6cb144c603829ca7bf86390de
SJR-Codes <109721383+SJR-Codes@users.noreply.github.com>
2022-07-31 08:16:10 +0000

    Sun, Jul 31, 2022, 8:16 AM +00:00

--- week_9/bank.py
+++ week_9/bank.py
@@ -4,19 +4,22 @@
 * by Samu Reinikainen 30.07.2022
 """
 
+#set variable to global
 balance = 0
 
 def main():
     print("Balance:", balance)
     deposit(100)
     print("Balance:", balance)
-    deposit(50)
+    withdraw(50)
     print("Balance:", balance)
 
 def deposit(n):
+    global balance
     balance += n
 
 def withdraw(n):
+    global balance
     balance -= n
 
 if __name__ == "__main__":
